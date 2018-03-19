084b222a4052a77b786bf3906a30d5a88a881393
Ben Kwok <ben.kwok.cy@gmail.com>
2018-03-19 16:55:37 -0700

    Add firebase init files

--- src/assets/js/signup.js
+++ src/assets/js/signup.js
@@ -1,37 +1,44 @@
-$(document)
-    .ready(function() {
-        $('.ui.form')
-        .form({
-            fields: {
-            username: {
-                identifier  : 'username',
-                rules: [
-                {
-                    type   : 'empty',
-                    prompt : 'Please enter your username'
-                }
-                ]
-            },
-            email: {
-                identifier  : 'email',
-                rules: [
-                {
-                    type   : 'email',
-                    prompt : 'Please enter a valid e-mail'
-                }
-                ]
-            },
-            password: {
-                identifier  : 'password',
-                rules: [
-                {
-                    type   : 'length[6]',
-                    prompt : 'Your password must be at least 6 characters'
-                }
-                ]
-            }
-            }
-        })
-        ;
-    })
-;
+function emptyUser() {
+    let string = document.getElementsByName("username")[0].value;
+    if (string.length === 0) { 
+        alert("Username is empty!");
+    } 
+}
+
+// $(document)
+//     .ready(function() {
+//         $('.ui.form')
+//         .form({
+//             fields: {
+//             username: {
+//                 identifier  : 'username',
+//                 rules: [
+//                 {
+//                     type   : 'empty',
+//                     prompt : 'Please enter your username'
+//                 }
+//                 ]
+//             },
+//             email: {
+//                 identifier  : 'email',
+//                 rules: [
+//                 {
+//                     type   : 'email',
+//                     prompt : 'Please enter a valid e-mail'
+//                 }
+//                 ]
+//             },
+//             password: {
+//                 identifier  : 'password',
+//                 rules: [
+//                 {
+//                     type   : 'length[6]',
+//                     prompt : 'Your password must be at least 6 characters'
+//                 }
+//                 ]
+//             }
+//             }
+//         })
+//         ;
+//     })
+// ;
