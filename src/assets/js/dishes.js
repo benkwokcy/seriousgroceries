d0c4a4aa7169c122cb8e7ebbd156eee7f7d06d09
Ben Kwok <ben.kwok.cy@gmail.com>
2018-03-20 01:22:00 -0700

    Allow dishes table to populate from external json

--- src/assets/js/dishes.js
+++ src/assets/js/dishes.js
@@ -1,10 +1,14 @@
-$(document)
-  .ready(function() {
 
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-    ;
+$(function(){
+  $.getJSON("assets/table.json", function(data) {
+    console.log('success');
+    $.each(data, function(i,dish) {
+      $('#table').append('<tr><td>' + data[i]["name"] + '</td><td>' + data[i]["ingredients"] + '</td></tr>');
+    });
+  });
+});
 
-  })
-;
+// function add() {
+//   var input = document.getElementsByClassName("ui.action.input");
+//   var text = input.innerHTML;
+// }
