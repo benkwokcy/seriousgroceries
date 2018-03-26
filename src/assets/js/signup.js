4842c26a9e7248424396d419c1946e280e472820
Ben Kwok <ben.kwok.cy@gmail.com>
2018-03-25 20:15:26 -0700

    Add partial sign up functionality to signup.html

--- src/assets/js/signup.js
+++ src/assets/js/signup.js
@@ -1,29 +1,47 @@
 'use strict';
 
 $(document).ready(function() {
+
+  // (function() {
+  //   FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+  //   if (user != null) {
+  //       // User is signed in
+  //   } else {
+  //       // No user is signed in
+  //   }
+  // }) ();
+
   $('.ui.form').form({
     fields: {
       username: {
         identifier  : 'username',
         rules: [
           {
-              type   : 'empty',
-              prompt : 'Please enter your username'
+            type   : 'empty',
+            prompt : 'Please enter a username'
           }
         ]
       },
       email: {
         identifier  : 'email',
         rules: [
-            {
-                type   : 'email',
-                prompt : 'Please enter a valid e-mail'
-            }
+          {
+            type   : 'empty',
+            prompt : 'Please enter your e-mail'
+          },
+          {
+            type   : 'email',
+            prompt : 'Please enter a valid e-mail'
+          }
         ]
       },
       password: {
         identifier  : 'password',
         rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your password'
+          },
           {
             type   : 'length[6]',
             prompt : 'Your password must be at least 6 characters'
@@ -32,11 +50,26 @@ $(document).ready(function() {
       }
     }
   });
-});
 
-// function emptyUser() {
-//     let string = document.getElementsByName("username")[0].value;
-//     if (string.length === 0) { 
-//         alert("Username is empty!");
-//     } 
-// }
+  document.querySelector('#signInButton').addEventListener('click', function(e) {
+      if( $('.ui.form').form('is valid')) {
+          var email, password;
+
+          e.preventDefault();
+          e.stopPropagation();
+          email = $('.ui.form').form('get value', 'email');
+          password = $('.ui.form').form('get value', 'password');
+          firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+              console.log(error.code);
+              console.log(error.message);
+          });
+      }
+  });
+
+  // document.querySelector('#sign-out').addEventListener('click', function(e) {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //     firebase.auth().signOut();
+  // });
+    
+});
