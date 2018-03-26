83ab4322d4d586b95733692ba505183e22f73d11
Ben Kwok <ben.kwok.cy@gmail.com>
2018-03-25 17:52:38 -0700

    Allow today cards to populate from json file

--- src/assets/js/today.js
+++ src/assets/js/today.js
@@ -1,12 +1,38 @@
 'use strict';
 
-$(document)
-  .ready(function() {
+// $(document)
+//   .ready(function() {
 
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-    ;
+//     // create sidebar and attach to menu open
+//     $('.ui.sidebar')
+//       .sidebar('attach events', '.toc.item')
+//     ;
 
-  })
-;
+//   })
+// ;
+
+// '<tr><td>' + data[i]["name"] + '</td><td>' + data[i]["ingredients"] + '</td></tr>'
+
+$(function(){
+  $.getJSON("assets/today.json", function(data) {
+    // console.log('success');
+    $.each(data, function(i,dish) {
+      $('#cards').append(
+        `<div class=\"ui card\">
+            <div class=\"image\">
+              <img src=\"` + data[i]["url"] + `\">
+            </div>
+            <div class=\"content\">
+              <div class=\"header\">` + data[i]["name"] + `</div>
+              <div class=\"meta\">
+                <a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
+              </div>
+              <div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
+          </div>
+          <div class=\"extra content\">
+            <a class=\"friends\">Available at: ` + data[i]["location"] + `</a>
+          </div>
+        </div>`);
+    });
+  });
+});
