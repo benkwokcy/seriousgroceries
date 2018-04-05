0393da6a9f3e05d96038328517a53c1f2a80e28c
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-04 21:23:18 -0700

    Add second list of cards to today.html

--- src/assets/js/today.js
+++ src/assets/js/today.js
@@ -29,7 +29,39 @@ $(document).ready(function() {
             <div class=\"ui inverted dimmer\">
               <div class=\"content\">
                 <div class=\"center\">
-                  <div class=\"ui primary button\">Details</div>
+                  <div class=\"ui blue button\">More</div>
+                </div>
+              </div>
+            </div>
+            <img src=\"` + data[i]["url"] + `\">
+          </div>
+
+          <div class=\"content\">
+            <div class=\"header\">` + data[i]["name"] + `</div>
+            <div class=\"meta\">
+              <a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
+            </div>
+            <div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
+          </div>
+
+          <div class=\"extra content\">
+            <a class=\"friends\">Available at: ` + data[i]["location"] + `</a>
+          </div>
+
+        </div>`);
+    });
+  }); 
+
+  $.getJSON("assets/quick.json", function(data) {
+    $.each(data, function(i,dish) {
+      $('#cards2').append(
+        `<div class=\"card\">
+
+          <div class=\"blurring image\">
+            <div class=\"ui inverted dimmer\">
+              <div class=\"content\">
+                <div class=\"center\">
+                  <div class=\"ui blue button\">More</div>
                 </div>
               </div>
             </div>
