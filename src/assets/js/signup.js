3da51835c8195792e8f5c535caea57eeffe05981
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-04 20:52:33 -0700

    Add dimmer to cards in today.html

--- src/assets/js/signup.js
+++ src/assets/js/signup.js
@@ -2,41 +2,27 @@
 
 $(document).ready(function() {
  
+  
   $('.ui.form').form({
     fields: {
       username: {
         identifier  : 'username',
         rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter a username'
-          }
+          {type   : 'empty', prompt : 'Please enter a username'}
         ]
       },
       email: {
         identifier  : 'email',
         rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter your e-mail'
-          },
-          {
-            type   : 'email',
-            prompt : 'Please enter a valid e-mail'
-          }
+          {type   : 'empty', prompt : 'Please enter your e-mail'},
+          {type   : 'email', prompt : 'Please enter a valid e-mail'}
         ]
       },
       password: {
         identifier  : 'password',
         rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter your password'
-          },
-          {
-            type   : 'length[6]',
-            prompt : 'Your password must be at least 6 characters'
-          }
+          {type   : 'empty', prompt : 'Please enter your password'},
+          {type   : 'length[6]', prompt : 'Your password must be at least 6 characters'}
         ]
       }
     }
@@ -57,12 +43,12 @@ $(document).ready(function() {
       }
   });
 
-//Handle Account Status
-firebase.auth().onAuthStateChanged(user => {
-  if(user) {
-    window.location = 'choice.html'; //After successful login, user will be redirected to home.html
-  }
-});
+  //Handle Account Status
+  firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      window.location = 'choice.html'; // After successful login, user will be redirected to home.html
+    }
+  });
 
   // document.querySelector('#sign-out').addEventListener('click', function(e) {
   //     e.preventDefault();
@@ -71,3 +57,51 @@ firebase.auth().onAuthStateChanged(user => {
   // });
     
 });
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+  // Listening for auth state changes.
+  // [START authstatelistener]
+  firebase.auth().onAuthStateChanged(function(user) {
+    // [START_EXCLUDE silent]
+    document.getElementById('quickstart-verify-email').disabled = true;
+    // [END_EXCLUDE]
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // [START_EXCLUDE]
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+      document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+      document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+      if (!emailVerified) {
+        document.getElementById('quickstart-verify-email').disabled = false;
+      }
+      // [END_EXCLUDE]
+    } else {
+      // User is signed out.
+      // [START_EXCLUDE]
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+      document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+      document.getElementById('quickstart-account-details').textContent = 'null';
+      // [END_EXCLUDE]
+    }
+    // [START_EXCLUDE silent]
+    document.getElementById('quickstart-sign-in').disabled = false;
+    // [END_EXCLUDE]
+  });
+  // [END authstatelistener]
+  document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+  document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
+  document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+  document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+}
--- src/assets/js/today.js
+++ src/assets/js/today.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   
   $('.ui.dropdown').dropdown();
 
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
@@ -14,25 +18,68 @@ $(document).ready(function() {
 
 });
 
-$(function(){
+// Populates table from JSON
+
   $.getJSON("assets/today.json", function(data) {
     $.each(data, function(i,dish) {
       $('#cards').append(
-        `<div class=\"ui card\">
-            <div class=\"image\">
-              <img src=\"` + data[i]["url"] + `\">
-            </div>
-            <div class=\"content\">
-              <div class=\"header\">` + data[i]["name"] + `</div>
-              <div class=\"meta\">
-                <a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
+        `<div class=\"card\">
+
+          <div class=\"blurring image\">
+            <div class=\"ui inverted dimmer\">
+              <div class=\"content\">
+                <div class=\"center\">
+                  <div class=\"ui primary button\">Details</div>
+                </div>
               </div>
-              <div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
+            </div>
+            <img src=\"` + data[i]["url"] + `\">
           </div>
+
+          <div class=\"content\">
+            <div class=\"header\">` + data[i]["name"] + `</div>
+            <div class=\"meta\">
+              <a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
+            </div>
+            <div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
+          </div>
+
           <div class=\"extra content\">
             <a class=\"friends\">Available at: ` + data[i]["location"] + `</a>
           </div>
+
         </div>`);
     });
   }); 
-});
+
+
+// Populates table from JSON
+      // $('#cards').append(
+      //   `<div class=\"card\">
+        
+      //   <div class=\"blurring dimmable image\">
+      //     <div class=\"ui dimmer\">
+      //       <div class=\"content\">
+      //         <div class=\"center\">
+      //           <div class=\"ui primary button\">Add Friend</div>
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <img src=\"https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg\">
+      //   </div>
+
+      //   <div class=\"content\">
+      //     <div class=\"header\">header</div>
+      //     <div class=\"meta\">
+      //       <a class=\"group\">SG Price Score: 123</a>
+      //     </div>
+      //     <div class=\"description\">Sale Items: Items</div>
+      //   </div>
+
+      //   <div class=\"extra content\">
+      //     <a class=\"friends\">Available at: Location</a>
+      //   </div>
+
+      // </div>`
+      // );
+
