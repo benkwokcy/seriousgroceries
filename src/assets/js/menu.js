fde2c98b39b69f66d5742e917ed4aafb5e3b60ed
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-10 22:46:31 -0700

    Change privacy.html menu

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -5,22 +5,22 @@ $(function() {
   
   // Change menu if logged in
   firebase.auth().onAuthStateChanged(function(user) {
-	if (user) {
-	  // User is signed in.
-	  $('#signin').hide();
-	  $('#signup').hide();
-	  $('#signout').show();
-	} else {
-	  // No user is signed in.
-	  $('#signout').hide();
-	  $('#signin').show();
-	  $('#signup').show();
-	}
-  });
-});
-
-// Sign out user when sign out button is clicked.
-$('#signout').on('click', function() {
-	firebase.auth().signOut();
-	alert("Log out successful");
+		if (user) {
+			// User is signed in.
+			$('#signin').hide();
+			$('#signup').hide();
+			$('#signout').show();
+		} else {
+			// No user is signed in.
+			$('#signout').hide();
+			$('#signin').show();
+			$('#signup').show();
+		}
+	});
+	
+	// Sign out user when sign out button is clicked.
+	$('#signout').on('click', function() {
+		firebase.auth().signOut();
+		alert("Log out successful");
+	});
 });
