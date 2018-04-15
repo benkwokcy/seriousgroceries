e995a6f462c9d3e44a3ab77f2343d9df905d387a
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-14 21:10:11 -0700

    Add select all dropdown to choice.html

--- src/assets/js/choice.js
+++ src/assets/js/choice.js
@@ -3,4 +3,23 @@
 // Run when DOM is ready
 $(function() {
 
+    $('.ui.dropdown').dropdown();
+
+    document.querySelector('#continue').addEventListener('click', function(e) {
+		// if( $('.ui.form').form('is valid')) {
+			var diet = $('#adropdown').dropdown('get value');
+			window.location = 'find.html';
+		// }
+	});
+
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+		  // User is signed in.
+		} else {
+		  // No user is signed in.
+		//   window.location = 'today.html';
+		}
+	  });
+	  
+
 });
