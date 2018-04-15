231a9fc03c60311d69ca853d363695962db36516
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-15 00:02:07 -0700

    Populate today from Firestore

--- src/assets/js/today.js
+++ src/assets/js/today.js
@@ -2,77 +2,104 @@
 
 // Run when DOM is ready
 $(function() {
-  
-  $('.ui.dropdown').dropdown();
+	
+	// Semantic UI Elements
+	$('.ui.dropdown').dropdown();
+	$('.special.cards .image').dimmer({
+		on: 'hover'
+	});
 
-  $('.special.cards .image').dimmer({
-    on: 'hover'
-  });
+	// Populate Tables 1 and 2 from Firestore
+	var db = firebase.firestore();
+	db.collection("sgRecipes").get().then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			// doc.data() is never undefined for query doc snapshots
+			// console.log(doc.id, " => ", doc.data());
+			populate(doc, 'cards');
+		});		
+	});
+	db.collection("userRecipes").get().then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			populate(doc, 'cards2');
+		});		
+	});
 
 });
 
+// Append card to table
+function populate(doc, id) {
+	$('#' + id).append(
+		`<div class=\"card\">
+
+			<div class=\"blurring image\">
+				<div class=\"ui inverted dimmer\">
+					<div class=\"content\">
+						<div class=\"center\">
+							<div class=\"ui blue button\">Save</div>
+						</div>
+					</div>
+				</div>
+				<img src=\"` + doc.data().img + `\">
+			</div>
+
+			<div class=\"content\">
+				<div class=\"header\">` + doc.data().name + `</div>
+				<div class=\"meta\">
+					<a class=\"group\">SG Price Score: ` + doc.data().score + `</a>
+				</div>
+				<div class=\"description\">Sale Items: ` + doc.data().ingredients + `</div>
+			</div>
+
+			<div class=\"extra content\">
+				<a class=\"friends\">Available at: ` + doc.data().location + `</a>
+			</div>
+
+	</div>`);
+}
+
 // Populates table 1 from JSON
-$.getJSON("assets/today.json", function(data) {
-  $.each(data, function(i,dish) {
-    $('#cards').append(
-      `<div class=\"card\">
+// $.getJSON("assets/today.json", function(data) {
+// 	$.each(data, function(i,dish) {
+// 		$('#cards').append(
+// 			`<div class=\"card\">
 
-        <div class=\"blurring image\">
-          <div class=\"ui inverted dimmer\">
-            <div class=\"content\">
-              <div class=\"center\">
-                <div class=\"ui blue button\">Save</div>
-              </div>
-            </div>
-          </div>
-          <img src=\"` + data[i]["url"] + `\">
-        </div>
+// 				<div class=\"blurring image\">
+// 					<div class=\"ui inverted dimmer\">
+// 						<div class=\"content\">
+// 							<div class=\"center\">
+// 								<div class=\"ui blue button\">Save</div>
+// 							</div>
+// 						</div>
+// 					</div>
+// 					<img src=\"` + data[i]["url"] + `\">
+// 				</div>
 
-        <div class=\"content\">
-          <div class=\"header\">` + data[i]["name"] + `</div>
-          <div class=\"meta\">
-            <a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
-          </div>
-          <div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
-        </div>
+// 				<div class=\"content\">
+// 					<div class=\"header\">` + data[i]["name"] + `</div>
+// 					<div class=\"meta\">
+// 						<a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
+// 					</div>
+// 					<div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
+// 				</div>
 
-        <div class=\"extra content\">
-          <a class=\"friends\">Available at: ` + data[i]["location"] + `</a>
-        </div>
+// 				<div class=\"extra content\">
+// 					<a class=\"friends\">Available at: ` + data[i]["location"] + `</a>
+// 				</div>
 
-    </div>`);
-  });
-}); 
+// 		</div>`);
+// 	});
+// }); 
 
-// Populate table 2 from JSON
-$.getJSON("assets/quick.json", function(data) {
-  $.each(data, function(i,dish) {
-    $('#cards2').append(
-      `<div class=\"card\">
-      
-        <div class=\"blurring image\">
-          <div class=\"ui inverted dimmer\">
-            <div class=\"content\">
-              <div class=\"center\">
-                <div class=\"ui blue button\">More</div>
-              </div>
-            </div>
-          </div>
-          <img src=\"` + data[i]["url"] + `\">
-        </div>
+// Populate database from JSON
+// $.getJSON("assets/quick.json", function(data) {
+// 	$.each(data, function(i,dish) {
+// 		db.collection("userRecipes").add(dish)
+// 		.then(function(docRef) {
+// 			console.log("Document written with ID: ", docRef.id);
+// 		})
+// 		.catch(function(error) {
+// 			console.error("Error adding document: ", error);
+// 		});
 
-        <div class=\"content\">
-          <div class=\"header\">` + data[i]["name"] + `</div>
-          <div class=\"meta\">
-            <a class=\"group\">SG Price Score: ` + data[i]["score"] + `</a>
-          </div>
-          <div class=\"description\">Sale Items: ` + data[i]["ingredients"] + `</div>
-        </div>
-
-        <div class=\"extra content\">
-          <a class=\"friends\">Available at: ` + data[i]["location"] + `</a>
-        </div>
-
-    </div>`);
-  });
-}); 
+// 	});
+// }); 
