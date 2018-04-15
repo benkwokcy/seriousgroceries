d23bbd2971162bd2cd1872441169d6a4188d2866
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-15 15:34:20 -0700

    Add datatable to dishes

--- src/assets/js/dishes.js
+++ src/assets/js/dishes.js
@@ -3,46 +3,62 @@
 // Run when DOM is ready
 $(function() {
 
-	// Populate Table from Firestore
+	// Populate DataTable from Firestore
 	var db = firebase.firestore();
-	populate("sgRecipes", db);
-	populate("userRecipes", db);
+	var table = $('#myTable').DataTable();
+	populate("sgRecipes", db, table);
+	populate("userRecipes", db, table);
+		
+	// Add new row to table
+	function populate(collection, db, table) {
+		db.collection(collection).get().then(function(querySnapshot) {
+			querySnapshot.forEach(function(doc) {
+				table.row.add( [
+					doc.data().name,
+					doc.data().ingredients,
+					doc.data().category,
+					'<a href="#"><i class="edit icon"></i></a>',
+					'<a href="#"><i class="trash icon"></i></a>'
+				] );
+			});		
+			table.draw();
+		});
+	}
 
 	// Show addDish modal menu when button is clicked
-	$("#addDish").click(function() {
+	$('#addDish').click(function() {
 		$('.ui.modal').modal({inverted: true}).modal('show');
 	});
 
   	// Semantic UI Elements
   	$('.ui.dropdown').dropdown();
-  	$('.ui.radio.checkbox').checkbox();
+	$('.ui.radio.checkbox').checkbox();
+	$('.ui.fluid.multiple.dropdown').dropdown({
+		allowAdditions: true
+	});
 
 	// Add new recipe
 	$("#create").click(function(event) {
 		// event.preventDefault()
 		let recipeName = $('.ui.form').form('get value', 'recipeName');
 		let recipeCategory = $('.ui.form').form('get value', 'recipeCategory');
-		let mainIngredients = $('.ui.form').form('get value', 'mainIngredients');
-		let secondaryIngredients = $('.ui.form').form('get value', 'secondaryIngredients');
-		// let visibility = $('.ui.form').form('is checked', 'private');
+		let ingredients = $('.ui.form').form('get value', 'ingredients');
+		let visibility = $('#dropdown').dropdown('set selected', value);
 		console.log(recipeName);
 		var dish = {name: recipeName, category: recipeCategory, ingredients: mainIngredients};
 		console.log(dish);
-		db.collection("createRecipes").add(dish)
-		.then(function(docRef) {
-			console.log("Document written with ID: ", docRef.id);
-		})
-		.catch(function(error) {
-			console.error("Error adding document: ", error);
-		});
+		// db.collection("createRecipes").add(dish)
+		// .then(function(docRef) {
+		// 	console.log("Document written with ID: ", docRef.id);
+		// })
+		// .catch(function(error) {
+		// 	console.error("Error adding document: ", error);
+		// });
+	});
+
+	// Clear form when cancelling
+	$("#cancel").click(function(event) {
+		$('form').form('clear');
 	});
   
 });
-
-function populate(collection, db) {
-	db.collection(collection).get().then(function(querySnapshot) {
-		querySnapshot.forEach(function(doc) {
-			$('#table').append('<tr><td>' + doc.data().name + '</td><td>' + doc.data().ingredients + '</td></tr>');
-		});		
-	});
-}
