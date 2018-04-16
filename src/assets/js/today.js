e746d0a054bb7b96837394a5ad218decd099f63b
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-16 01:05:35 -0700

    Populate today lists from FireStore

--- src/assets/js/today.js
+++ src/assets/js/today.js
@@ -11,48 +11,44 @@ $(function() {
 
 	// Populate Tables 1 and 2 from Firestore
 	var db = firebase.firestore();
-	db.collection("sgRecipes").get().then(function(querySnapshot) {
-		querySnapshot.forEach(function(doc) {
-			// doc.data() is never undefined for query doc snapshots
-			// console.log(doc.id, " => ", doc.data());
-			populate(doc, 'cards');
-		});		
-	});
-	db.collection("userRecipes").get().then(function(querySnapshot) {
-		querySnapshot.forEach(function(doc) {
-			populate(doc, 'cards2');
-		});		
-	});
+	populate(db, "Quick", "cards");
+	populate(db, "Thanksgiving", "cards2");
 
 });
 
 // Append card to table
-function populate(doc, id) {
-	$('#' + id).append(
-		`<div class=\"card\">
+function populate(db, category, id) {
+	db.collection("recipes").where("category", "==", category).orderBy("score", "desc").get().then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			// doc.data() is never undefined for query doc snapshots
+			// console.log(doc.id, " => ", doc.data());
+			$('#' + id).append(
+				`<div class=\"card\">
 
-			<div class=\"blurring image\">
-				<div class=\"ui inverted dimmer\">
-					<div class=\"content\">
-						<div class=\"center\">
-							<div class=\"ui blue button\">Save</div>
+					<div class=\"blurring image\">
+						<div class=\"ui inverted dimmer\">
+							<div class=\"content\">
+								<div class=\"center\">
+									<div class=\"ui blue button\">Save</div>
+								</div>
+							</div>
 						</div>
+						<img src=\"` + doc.data().img + `\">
 					</div>
-				</div>
-				<img src=\"` + doc.data().img + `\">
-			</div>
 
-			<div class=\"content\">
-				<div class=\"header\">` + doc.data().name + `</div>
-				<div class=\"meta\">
-					<a class=\"group\">SG Price Score: ` + doc.data().score + `</a>
-				</div>
-				<div class=\"description\">Sale Items: ` + doc.data().ingredients + `</div>
-			</div>
+					<div class=\"content\">
+						<div class=\"header\">` + doc.data().name + `</div>
+						<div class=\"meta\">
+							<a class=\"group\">SG Price Score: ` + doc.data().score + `</a>
+						</div>
+						<div class=\"description\">Sale Items: ` + doc.data().ingredients + `</div>
+					</div>
 
-			<div class=\"extra content\">
-				<a class=\"friends\">Available at: ` + doc.data().location + `</a>
-			</div>
+					<div class=\"extra content\">
+						<a class=\"friends\">Available at: ` + doc.data().location + `</a>
+					</div>
 
-	</div>`);
+			</div>`);
+		});		
+	});
 }
