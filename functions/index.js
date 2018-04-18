915ee7ef877bc2444757527b1e3b3df0c89225ca
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-17 23:33:06 -0700

    Add scrape function for Thrifty Foods

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,44 @@
+/**
+ * author: Benjamin Kwok
+ */
+
+'use strict';
+
+/**********  IMPORTS  **********/
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
+// The Firebase Admin SDK
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+/**********  WEB SCRAPER  **********/
+// Scrape Thrifty Food JSON flyer from Flipp
+exports.scrape = functions.https.onRequest((request, response) => {
+
+    var db = admin.firestore();
+    var url = "https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code=V8N2P5&q=Thrifty Foods"
+    var request = require('request');
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+         var flyer = JSON.parse(body);   
+        for (let item in flyer.items) {
+            db.collection("flyersTest").add({
+                name: flyer.items[item].name,
+                valid_to: flyer.items[item].valid_to,
+                valid_from: flyer.items[item].valid_from,
+                store_name: flyer.items[item].merchant_name
+            })
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+        }
+      }
+    })
+
+    response.send("Scrape complete!");
+    
 });
