c4c48bdf5114f69c9385a0a95c88daebd9fa07c3
Ben Kwok <ben.kwok.cy@gmail.com>
2018-04-17 23:34:11 -0700

    Add create dish function to modal

--- src/assets/js/dishes.js
+++ src/assets/js/dishes.js
@@ -3,11 +3,17 @@
 // Run when DOM is ready
 $(function() {
 
+	// Redirect unlogged in users
+	// firebase.auth().onAuthStateChanged(user => {
+	// 	if(!user) {
+	// 		window.location = 'signin.html'; 
+	// 	}
+	// });
+
 	// Populate DataTable from Firestore
 	var db = firebase.firestore();
 	var table = $('#myTable').DataTable();
-	populate("sgRecipes", db, table);
-	populate("userRecipes", db, table);
+	populate("recipes", db, table);
 		
 	// Add new row to table
 	function populate(collection, db, table) {
@@ -25,11 +31,6 @@ $(function() {
 		});
 	}
 
-	// Show addDish modal menu when button is clicked
-	$('#addDish').click(function() {
-		$('.ui.modal').modal({inverted: true,closable: false}).modal('show');
-	});
-
   	// Semantic UI Elements
   	$('.ui.dropdown').dropdown();
 	$('.ui.radio.checkbox').checkbox();
@@ -37,34 +38,55 @@ $(function() {
 		allowAdditions: true
 	});
 
+	// Show addDish modal menu when button is clicked
+	$('#addDish').click(function() {
+		$('.ui.modal').modal({inverted: true,closable: false}).modal('show');
+	});
+
 	// Add new recipe
 	$("#create").click(function(event) {
-		let name = $('.ui.form').form('get value', 'recipeName');
-		let category = $('.ui.form').form('get value', 'recipeCategory');
-		let ingredients = $('.ui.form').form('get value', 'ingredients');
-		let shared = $('.ui.checkbox').checkbox('is checked')[0] === false;
-		var dish = {name: name, ingredients: ingredients, category: category, shared: shared};
+		if( $('.ui.form').form('is valid')) {
+			let name = $('.ui.form').form('get value', 'recipeName');
+			let category = $('.ui.form').form('get value', 'recipeCategory');
+			let ingredients = $('.ui.form').form('get value', 'ingredients');
+			let shared = $('.ui.checkbox').checkbox('is checked')[0] === false;
+			var dish = {name: name, ingredients: ingredients, category: category, shared: shared};
 
-		db.collection("recipes").add(dish)
-		.then(function(docRef) {
-			console.log("Document written with ID: ", docRef.id);
-			table.row.add( [
-				dish.name,
-				dish.ingredients,
-				dish.category,
-				'<a href="#"><i class="edit icon"></i></a>',
-				'<a href="#"><i class="trash icon"></i></a>' 
-			] ).draw();
-		})
-		.catch(function(error) {
-			console.error("Error adding document: ", error);
-			alert("Could not create recipe.");
-		});
+			db.collection("recipes").add(dish)
+			.then(function(docRef) {
+				console.log("Document written with ID: ", docRef.id);
+				table.row.add( [
+					dish.name,
+					dish.ingredients,
+					dish.category,
+					'<a href="#"><i class="edit icon"></i></a>',
+					'<a href="#"><i class="trash icon"></i></a>' 
+				] ).draw();
+				$('form').form('clear');
+			})
+			.catch(function(error) {
+				console.error("Error adding document: ", error);
+				alert("Could not create recipe.");
+			});
+		} else {
+			event.preventDefault();
+			event.stopPropagation();
+			$('.ui.form').form('validate form');
+		}
 	});
 
 	// Clear form when cancelling
 	$("#cancel").click(function(event) {
 		$('form').form('clear');
+	});
+
+	// Set validation rules for form
+	$('.ui.form').form({
+		fields: {
+			recipeName: 'empty',
+			recipeCategory: 'empty',
+			ingredients: 'empty'
+		}
 	});
   
 });
